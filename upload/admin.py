bf0127f17835b3f2d22e4c2daecc8fe7f320dd09
eapu <elena.andresprado@gmail.com>
2020-01-21 11:32:58 +0000

    fuu

--- upload/admin.py
+++ upload/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import Usuarios
+from django.utils.html import format_html
+from imagekit.admin import AdminThumbnail
 
 class UsuariosAdmin(admin.ModelAdmin):
     list_display = ('user','totem','image_tag','uploaded_at')
     list_filter = ('user','totem', 'upload','uploaded_at')
 
+    def thumbnail(self, obj):
+        return format_html('<img src="{}" style="width: 130px; \
+                           height: 100px"/>'.format(obj.url_image))
+
+    thumbnail.short_description = 'thumbnail'
 
 admin.site.register(Usuarios, UsuariosAdmin)
