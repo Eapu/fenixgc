73a7a546071be9d5c43be08b1481072f7d1df20f
eapu <elena.andresprado@gmail.com>
2020-01-21 11:19:20 +0000

    fuu

--- upload/admin.py
+++ upload/admin.py
@@ -5,5 +5,10 @@ class UsuariosAdmin(admin.ModelAdmin):
     list_display = ('user','totem','image_tag','uploaded_at')
     list_filter = ('user','totem', 'upload','uploaded_at')
 
+    def thumbnail(self, obj):
+        return format_html('<img src="{}" style="width: 130px; \
+                           height: 100px"/>'.format(obj.url_image))
+
+    thumbnail.short_description = 'thumbnail'
 
 admin.site.register(Usuarios, UsuariosAdmin)
