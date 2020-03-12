28324c83b01d58108dd9dbd5604b25924c876794
Eapu <elena.andresprado@gmail.com>
2020-03-12 14:56:28 +0000

    add changes

--- upload/forms.py
+++ upload/forms.py
@@ -24,6 +24,7 @@ class UsuariosForm(forms.ModelForm):
 	user = forms.CharField(max_length=10,label='Nombre', help_text='')
 	totem= forms.CharField(widget=forms.Select(choices=TOTEM_CHOICES), help_text='Seleciona el totem en el que te encuentres.')
 	upload = forms.FileField(widget=forms.FileInput(),label='Imagen', help_text='Sube la foto!')
+	email = forms.CharField(required=False,max_length=10,label='Email', help_text='Envianos tu mail si quieres participar en nuestro sorteo!')
 
 	class Meta:
 		model = Usuarios
@@ -32,6 +33,8 @@ class UsuariosForm(forms.ModelForm):
 			'user': ('Nombre'),
 			'totem': ('totem'),
 			'upload': ('imagen'),
+			'email': ('email'),
+
 		 }
 		
 
