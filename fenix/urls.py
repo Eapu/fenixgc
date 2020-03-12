78bb4bee2bce7c6757483a72d2ce23eff83282e9
Eapu <elena.andresprado@gmail.com>
2020-03-12 02:55:00 +0000

    add changes

--- fenix/urls.py
+++ fenix/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+admin.site.site_header = 'Fenix Gran Canaria'                    # default: "Django Administration"
+admin.site.index_title = 'Fenix'                 # default: "Site administration"
+admin.site.site_title = 'Fenix' # default: "Django site admin"
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('green.urls')),
