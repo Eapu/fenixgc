718470afc2751c6f1b6b29f37567a1dcfc0c2d9d
Eapu <elena.andresprado@gmail.com>
2020-04-24 02:21:37 +0100

    add changes

--- fenix/settings.py
+++ fenix/settings.py
@@ -87,11 +87,14 @@ WSGI_APPLICATION = 'fenix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'elena',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
